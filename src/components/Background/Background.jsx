eb6084a9fd81f47becf6af3ff657160c941d6a2f
K.Kishin <riesgraf.birsack.1226@gmail.com>
2022-03-13 21:53:45 +0900

    add image file

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import './Background.scss';
 import mobileLightBackground from '../../assets/bg-mobile-light.jpg';
-// import mobileDarkBackground from './assets/bg-mobile-dark.jpg';
+import mobileDarkBackground from '../../assets/bg-mobile-dark.jpg';
 import desktopLightBackground from '../../assets/bg-desktop-light.jpg';
-// import desktopDarkBackground from './assets/bg-desktop-dark.jpg';
+import desktopDarkBackground from '../../assets/bg-desktop-dark.jpg';
 
-const Background = () => {
+const Background = ({ theme }) => {
 	return (
 		<>
 			<div className="background">
 				<div className="background__content">
 					<picture className="background__image">
-						<source media="(min-width: 36em)" srcSet={desktopLightBackground} />
-						<img src={mobileLightBackground} alt="background" />
+						<source
+							media="(min-width: 36em)"
+							srcSet={`${
+								theme === 'light-theme'
+									? desktopLightBackground
+									: desktopDarkBackground
+							}`}
+						/>
+						<img
+							src={`${
+								theme === 'light-theme'
+									? mobileLightBackground
+									: mobileDarkBackground
+							}`}
+							alt="background"
+						/>
 					</picture>
 				</div>
 			</div>
