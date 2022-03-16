53a00d1e0429ff814d39806ee1c10d8ea62a906f
K.Kishin <riesgraf.birsack.1226@gmail.com>
2022-03-16 13:13:41 +0900

    add toString method

--- src/initialTodos.js
+++ src/initialTodos.js
@@ -1,11 +1,15 @@
 import { v1 as uuid } from 'uuid';
 
 export const initialTodos = [
-	{ id: uuid(), isComplete: true, task: 'Complete online JavaScript course' },
-	{ id: uuid(), isComplete: false, task: 'Jog around the park 3x' },
-	{ id: uuid(), isComplete: false, task: '10 minutes meditation' },
-	{ id: uuid(), isComplete: false, task: 'Read for 1 hour' },
-	{ id: uuid(), isComplete: false, task: 'Pick up grocies' },
+	{
+		id: uuid().toString(),
+		isComplete: true,
+		task: 'Complete online JavaScript course',
+	},
+	{ id: uuid().toString(), isComplete: false, task: 'Jog around the park 3x' },
+	{ id: uuid().toString(), isComplete: false, task: '10 minutes meditation' },
+	{ id: uuid().toString(), isComplete: false, task: 'Read for 1 hour' },
+	{ id: uuid().toString(), isComplete: false, task: 'Pick up grocies' },
 	{
 		id: uuid(),
 		isComplete: false,
